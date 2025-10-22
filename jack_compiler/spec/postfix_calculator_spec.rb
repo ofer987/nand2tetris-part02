80beff4aa6b0035fd2f13ada1c9d3a3f09594e00
Dan Jakob Ofer <dan@ofer.to>
2025-10-22 15:48:30 -0400

    test(postfix-calculator): aggregate failures

--- jack_compiler/spec/postfix_calculator_spec.rb
+++ jack_compiler/spec/postfix_calculator_spec.rb
@@ -5,49 +5,27 @@ require_relative '../lib/jack_compiler'
 RSpec.describe JackCompiler::PostfixCalculator do
   subject { described_class }
 
-  context '#initialize' do
-    context 'infix_expression' do
-      it 'returns an object' do
-        expect(subject.new(expression: '3 + 4')).to be_instance_of(subject)
-      end
-    end
-
-    context 'expression' do
-      it 'returns an object' do
-        expect(subject.new(expression: '3 4 +')).to be_instance_of(subject)
-      end
+  describe '#initialize', :agg do
+    it 'returns an object', :aggregate_failres do
+      expect(subject.new(expression: '3 + 4')).to be_instance_of(subject)
+      expect(subject.new(expression: '3 4 +')).to be_instance_of(subject)
     end
   end
 
   describe '#calculate' do
-    context 'when infix_expression is 6 + 7' do
-      it 'is 13' do
-        expect(subject.new(infix_expression: '6 + 7').calculate).to eq(13)
-      end
+    it 'calculates infix_expressions', :aggregate_failres do
+      expect(subject.new(infix_expression: '6 + 7').calculate).to eq(13)
+      expect(subject.new(infix_expression: '3 + 4').calculate).to eq(7)
+      expect(subject.new(infix_expression: '3 + 4 / 5 * (6 + 7) - 8').calculate).to eq(-5)
     end
 
-    context 'when infix_expression is 3 + 4' do
-      it 'is 7' do
-        expect(subject.new(infix_expression: '3 + 4').calculate).to eq(7)
-      end
+    it 'calculates expressions', :aggregate_failres do
+      expect(subject.new(expression: '3 4 5 / 6 7 + * + 8 -').calculate).to eq(-5)
     end
 
-    context 'when infix_expression is 3 + 4 / 5 * (6 + 7) - 8' do
-      it 'is -5' do
-        expect(subject.new(infix_expression: '3 + 4 / 5 * (6 + 7) - 8').calculate).to eq(-5)
-      end
-    end
-
-    context 'when expression is 3 4 5 / 6 7 + * + 8 -' do
-      it 'is -5' do
-        expect(subject.new(expression: '3 4 5 / 6 7 + * + 8 -').calculate).to eq(-5)
-      end
-    end
-
-    context 'when infix_expression is 1 + index ' do
-      it 'raises an exception' do
-        expect { subject.new(infix_expression: '1 + index').calculate }.to raise_error 'Variables are not implemented yet'
-      end
+    it 'raises error if expression contains variable', :aggregate_failres do
+      expect { subject.new(infix_expression: '1 + index').calculate }.to raise_error 'Variables are not implemented yet'
+      expect { subject.new(expression: '1 index +').calculate }.to raise_error 'Variables are not implemented yet'
     end
   end
 end
