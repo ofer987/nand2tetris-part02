9e32a8f5543b58dd57c042b69ca7382a0e0e53a1
Dan Jakob Ofer <dan@ofer.to>
2026-01-26 10:26:58 -0500

    chore(field-memory): implementation

--- jack_compiler/lib/jack_compiler/class_field_variable_node.rb
+++ jack_compiler/lib/jack_compiler/class_field_variable_node.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module JackCompiler
-  class ClassVariableNode < VariableNode
-    NODE_NAME = Statement::CLASS_VAR_DESCRIPTION
+  class ClassFieldVariableNode < VariableNode
+    NODE_NAME = Statement::CLASS_FIELD_VAR_DESCRIPTION
 
     def kind
-      Memory::Kind::STATIC
+      Memory::Kind::FIELD
     end
 
     def initialize(xml_node, options = {})
---
+++ jack_compiler/lib/jack_compiler/class_field_variable_statement.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module JackCompiler
+  class ClassFieldVariableStatement < Statement
+    REGEX = RegularExpressions::CLASS_FIELD_VAR_STATEMENT
+
+    def create_elements(parent_node, lines)
+      result = lines.match(REGEX)
+      result_node = document.create_element(CLASS_FIELD_VAR_DESCRIPTION)
+
+      parent_node << result_node
+
+      keyword_node = document.create_element(KEYWORD, result[1])
+      result_node << keyword_node
+
+      keyword_node = document.create_element(KEYWORD, result[2])
+      result_node << keyword_node
+
+      identifier_node = document.create_element(IDENTIFIER, result[3])
+      result_node << identifier_node
+
+      symbol_node = document.create_element(SYMBOL, SEMI_COLON)
+      result_node << symbol_node
+
+      lines.sub(REGEX, '')
+    end
+  end
+end
