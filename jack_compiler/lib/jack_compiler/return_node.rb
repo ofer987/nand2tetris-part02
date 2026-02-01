cf0af37b844b8726a91ff6c0b9eef602a104051e
Dan Jakob Ofer <dan@ofer.to>
2026-02-01 12:03:11 -0500

    chore(return-node): return this and memory

--- jack_compiler/lib/jack_compiler/return_node.rb
+++ jack_compiler/lib/jack_compiler/return_node.rb
@@ -3,27 +3,42 @@
 module JackCompiler
   class ReturnNode < Node
     NODE_NAME = Statement::RETURN_STATEMENT
+    OBJECT_POINTER = 'this'
 
     attr_reader :value
 
     def initialize(xml_node, options = {})
       super(xml_node, options)
 
-      self.value = "> #{Statement::IDENTIFIER}"
+      self.value = Statement::IDENTIFIER
+      self.memory_scope = options[:memory_scope]
     end
 
     def emit_vm_code
-      <<~VM_CODE
-        push constant #{value}
-        return
-      VM_CODE
+      if value == OBJECT_POINTER
+        <<~VM_CODE
+          push pointer 0
+          return
+        VM_CODE
+      elsif value.match?(/^\d+$/)
+        <<~VM_CODE
+          push constant #{value}
+          return
+        VM_CODE
+      else
+        memory_scope[value].read_memory
+      end
     end
+
+    private
 
     def value=(val)
       value_node = find_child_nodes(val).first
 
-      @value = 0
+      @value = '0'
       @value = value_node.text.strip unless value_node.nil?
     end
+
+    attr_accessor :memory_scope
   end
 end
