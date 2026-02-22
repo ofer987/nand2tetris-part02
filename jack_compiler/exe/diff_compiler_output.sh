2ab1eab5cb76e0c80c4d456b14047fbd310c9ce7
Dan Jakob Ofer <dan@ofer.to>
2026-02-22 10:51:08 -0500

    test(tools): test directories of Jack files

--- jack_compiler/exe/diff_compiler_output.sh
+++ jack_compiler/exe/diff_compiler_output.sh
@@ -5,8 +5,7 @@ PROVIDED_PROJECT_COMPILER='/Users/ofer987/me/nand2Tetris_partTwo/nand2tetris/too
 MY_PROJECT_COMPILER='/Users/ofer987/me/nand2Tetris_partTwo/jack_compiler/exe/compiler'
 
 JACK_PATH=$1
-VM_CODE_PATH=$(echo $JACK_PATH | sed s/\.jack/\.vm/)
+VM_CODE_PATH="${1}/*.vm"
 
 $PROVIDED_PROJECT_COMPILER $JACK_PATH
-
-diff --color $VM_CODE_PATH <($MY_PROJECT_COMPILER $JACK_PATH)
+diff --color <(cat $VM_CODE_PATH) <($MY_PROJECT_COMPILER $JACK_PATH)
