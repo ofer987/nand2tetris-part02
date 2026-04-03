611fa08f8a7b9895887e455b0926bacd90a10afe
Dan Jakob Ofer <dan@ofer.to>
2026-04-03 10:51:12 -0400

    fix(testing-tool): compile one file

--- jack_compiler/exe/diff_compiler_output.sh
+++ jack_compiler/exe/diff_compiler_output.sh
@@ -2,7 +2,7 @@
 
 PROVIDED_PROJECT_COMPILER='/Users/ofer987/me/nand2Tetris_partTwo/nand2tetris/tools/JackCompiler.sh'
 
-MY_PROJECT_COMPILER='/Users/ofer987/me/nand2Tetris_partTwo/jack_compiler/exe/compiler'
+MY_PROJECT_COMPILER='/Users/ofer987/me/nand2Tetris_partTwo/jack_compiler/exe/file_compiler'
 
 JACK_PATH=$1
 VM_CODE_PATH="${1}/*.vm"
