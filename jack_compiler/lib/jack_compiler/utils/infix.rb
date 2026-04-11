06ace6a345d14f43bd57264a35fda8285abdf503
Dan Jakob Ofer <dan@ofer.to>
2026-04-11 15:27:03 -0400

    fix(infix-evaluator): fix array operand regex

--- jack_compiler/lib/jack_compiler/utils/infix.rb
+++ jack_compiler/lib/jack_compiler/utils/infix.rb
@@ -12,7 +12,7 @@ module JackCompiler
       MONOMIAL_REGEX_OPEN_ROUND_BRACKET = /^\s*([+~-])\s*(\()\s*/
       OPERATOR_REGEX = /^\s*(#{OPERATORS_LIST_REGEX})\s*/
       COMPARISON_OPERATOR_REGEX = /^\s*(#{COMPARISON_OPERATORS_LIST_REGEX})\s*/
-      ARRAY_OPERAND_REGEX = /^\s*(([\w.]+)\[(.+)\])\s*/
+      ARRAY_OPERAND_REGEX = /^\s*(([\w.]+)\[([^\]]+)\])\s*/
       BOOLEAN_CONSTANT_REGEX = /(false|true)/
       NUMERICAL_REGEX = /^\s*(\d+)\s*/
       OPERAND_REGEX = /^\s*([\w.]+)\s*/
