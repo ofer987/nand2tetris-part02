2797c090b4092662dfb57cd30c409f1bf2b5e13f
Dan Jakob Ofer <dan@ofer.to>
2026-04-11 15:25:23 -0400

    fix(do-statement): add NODE_NAME

--- jack_compiler/lib/jack_compiler/do_statement.rb
+++ jack_compiler/lib/jack_compiler/do_statement.rb
@@ -2,6 +2,7 @@
 
 module JackCompiler
   class DoStatement < Statement
+    NODE_NAME = DO_STATEMENT
     REGEX = RegularExpressions::DO_STATEMENT
 
     def create_elements(parent_node, lines)
@@ -10,7 +11,7 @@ module JackCompiler
         raise "Cannot instantiate new instance of '#{result[3]}' in '#{result[0]}'"
       end
 
-      result_node = document.create_element(REGEX)
+      result_node = document.create_element(DO_STATEMENT)
 
       parent_node << result_node
 
